d1479aaeaa88e7336f2be6ee21fca750cea698d2
GarmaNazareno <garmatorresnazareno@gmail.com>
2022-09-06 20:43:22 -0300

    oneofall

--- server/DAO/index.js
+++ server/DAO/index.js
@@ -1,40 +1,18 @@
-// const daoArchiveCart = require("./carts/daoArchive");
-// const daoArchiveProducts = require("./products/daoArchive");
+require("dotenv").config();
 const DaoMongoProductsContainer = require("./products/daoMongo");
 const DaoMongoCartContainer = require("./carts/daoMongo");
 const DaoMongoUsersContainer = require("./users/daoMongo");
-// const DaoFirebaseProducts = require("./products/daoFirebase");
-// const DaoFirebaseCart = require("./carts/daoFirebase");
-// const DaoMySqlProductsContainer = require("./products/daoMySql");
-// const DaoMySqlCartContainer = require("./carts/daoMySql");
 
 const getStorage = () => {
   const storage = process.env.STORAGE;
 
   switch (storage) {
-    case "firebase":
-      return {
-        products: new DaoFirebaseProducts(),
-        cart: new DaoFirebaseCart(),
-      };
     case "mongo":
       return {
         products: new DaoMongoProductsContainer(),
         cart: new DaoMongoCartContainer(),
         users: new DaoMongoUsersContainer(),
       };
-    case "fs":
-      return {
-        products: new daoArchiveProducts(),
-        cart: new daoArchiveCart(),
-      };
-    case "mysql":
-      return {
-        products: new DaoMySqlProductsContainer(),
-        cart: new DaoMySqlCartContainer(),
-      };
-    default:
-      throw new Error("Storage not found");
   }
 };
 
