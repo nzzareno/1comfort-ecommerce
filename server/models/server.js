ab73f2ecb45aa2fd41fc9f277d2d6c5f8f3a597e
GarmaNazareno <garmatorresnazareno@gmail.com>
2022-11-03 01:02:25 -0300

    finaz

--- server/models/server.js
+++ server/models/server.js
@@ -49,13 +49,13 @@ class Sv {
     this.app.use(cors());
     this.app.use(express.json());
     this.app.use(express.urlencoded({ extended: true }));
-    this.app.use(express.static(path.join(__dirname, "build")));
+    this.app.use(express.static('../../client/build/index.html'));
     this.app.use(morgan("dev"));
   }
   routes() {
     this.app.get("*", function (req, res) {
-      const index = path.join(__dirname, "build", "index.html");
-      res.sendFile(index);
+      res.sendFile(path.resolve(__dirname, "../../client/build/index.html"));
+ 
     });
     this.app.use(this.productsRoute, productRouter);
     this.app.use(this.cartRoute, cartRouter);
